076b43f1bdcdf970398926d9afdcdcc5abfc982e
alfa23 <paw235@gmail.com>
2022-06-12 19:14:40 +0000

    Remove settings.py .py SECRET_KEY & add new as GitPod environment var

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%-&2t^!1_=j^&w@6o1kmew2-!vz)e282ck-lfx@8h63q0u@jh9')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
